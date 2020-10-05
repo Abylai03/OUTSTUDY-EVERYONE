57fe1ebcf2eee218f1d53a58474bf218647cb3b9
Abylai03 <61981188+Abylai03@users.noreply.github.com>
2020-10-05 22:55:47 +0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var Swiper = new Swiper(".swiper-container2", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
